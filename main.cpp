78d07d30f8da137a3017428240e3de6d1b9f6783
David Oberhofer <oberhofer.david@gmail.com>
2017-09-13 22:08:10 +0200

    initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Balls-Controller.cpp
+ *
+ * Created: 13.09.2017 22:02:41
+ * Author : David
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
